98a3761b0978e54138aebb041678b07a054e8997
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-04 20:13:18 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,19 +39,6 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map(nav => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
       </div>
     </nav>
   )
