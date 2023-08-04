77a37dbaf648e7cbc3e040d45c82f65694a64177
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-04 21:34:40 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor cursor-pointer">
-            Mohammad |{" "}
+            Mohammad &nbsp;
             <span className="sm:block hidden">Software Developer</span>
           </p>
         </Link>
