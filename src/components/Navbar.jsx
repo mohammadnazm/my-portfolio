ab3feac6ea5be9e56ca36419438ddf0489f0f00e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-04 20:20:41 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { logo, menu, close } from "../assets"
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState("")
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -39,6 +41,14 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   )
