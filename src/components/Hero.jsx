80d639a362fac3d6565f0c48caceb5ae86efedea
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-04 22:49:29 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
       </div>
     </section>
