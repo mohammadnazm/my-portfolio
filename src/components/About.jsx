8d87cbffb1fc5be11ddf2ae4ae0a99ab866e15ed
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 16:44:55 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,16 @@ const ServiceCard = ({ index, title, icon }) => {
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-      ></motion.div>
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        ></div>
+      </motion.div>
     </Tilt>
   )
 }
