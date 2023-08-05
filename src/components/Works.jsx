f602e6b001338506bad3ce6ed1631873ff906ac3
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 23:03:38 +0300

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+// import Tilt from "react-tilt"
+import { motion } from "framer-motion"
+
+import { styles } from "../styles"
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { fadeIn, textVariant } from "../utils/motion"
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.dev variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
+      </motion.dev>
+    </>
   )
 }
 
