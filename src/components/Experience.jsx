a6f98279458b65fe134bd690556dbca2d38d3930
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 18:53:47 +0300

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience, index) => (
-            <ExperienceCard key={index} experiment={experience} />
+            <ExperienceCard experiment={experience} key={index} />
           ))}
         </VerticalTimeline>
       </div>
