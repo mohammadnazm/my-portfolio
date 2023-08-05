667f7b28065c090b427a389c3f57f5fb568ba4e5
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 10:01:35 +0300

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,33 @@ import CanvasLoader from "../Loader"
 
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
-  return <div>Computers</div>
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5] }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+    </Canvas>
+  )
 }
 
 export default Computers
