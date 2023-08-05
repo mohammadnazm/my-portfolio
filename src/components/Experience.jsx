89e705c48f61d5b9ec23b9eb42522b91828d69fd
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 19:25:24 +0300

    upgrade

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -31,7 +31,24 @@ const ExperienceCard = ({ experience }) => {
         </div>
       }
     >
-      <div className="text-white text-[24px] font-bold">{experience.title}</div>
+      <div className="text-white text-[24px] font-bold">
+        {experience.title}
+        <p
+          className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index} className="text-white-100 text-[14px] pl-1 tracking-wider`}
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
     </VerticalTimelineElement>
   )
 }
