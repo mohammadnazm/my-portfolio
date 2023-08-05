8cd71e6614443ae40f6488d9c2b64bff17b3b4b7
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 17:32:09 +0300

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react"
+import {
+  VerticalTimeline,
+  VerticalTimeLineElement,
+} from "react-vertical-timeline-component"
+
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { experiences } from "../constants"
+import { SectionWrapper } from "../hoc"
+import { textVariant } from "../utils/motion"
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.dev variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
+      </motion.dev>
+    </>
   )
 }
 
