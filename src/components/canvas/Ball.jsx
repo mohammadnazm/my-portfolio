254278da203737f5e906fc93b7924ac63a511948
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-05 20:25:22 +0300

    upgrade

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,19 +9,43 @@ import {
 } from "@react-three/drei"
 
 import CanvasLoader from "../Loader"
-import Computers from "./Computers"
 
 const Ball = props => {
   const [decal] = useTexture([props.imgUrl])
-  return <div>Ball</div>
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  )
 }
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Computers isMobile={isMobile} />
         <Ball imgUrl={icon} />
       </Suspense>
 
@@ -30,4 +54,4 @@ const BallCanvas = ({ icon }) => {
   )
 }
 
-export default Ball
+export default BallCanvas
