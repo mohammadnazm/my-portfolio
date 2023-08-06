ec1f8e79163a0de05647ac1ffe0a92525acb3a03
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 11:48:04 +0300

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Suspense } from "react"
+import { Canvas } from "react-three-fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+
+import CanvasLoader from "../Loader"
 
 const Earth = () => {
+  return <div>Earth</div>
+}
+
+const EarthCanvas = () => {
   return (
-    <div>Earth</div>
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
   )
 }
 
