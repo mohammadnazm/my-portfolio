993ea7dd91c4dc13c4205eb2f49d0c03d11776ae
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 10:42:17 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,21 @@ const Contact = () => {
         ref={formRef}
         onSubmit={handleSubmit}
         className="mt-12 flex flex-col gap-8"
-      ></form>
+      >
+        <label className="flex flex-col">
+          <span className="flex flex-col">
+            <span className="text-white font-medium mb-4">Mohammad Nazm</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name"
+              className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            ></input>
+          </span>
+        </label>
+      </form>
     </div>
   )
 }
