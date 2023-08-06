c5f3e243815c4be716bf951fe5c663a189cf7fc9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 17:14:55 +0300

    Upgrade

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, Suspense } from "react"
-import { Canvas, useFrame } from "react-three-fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
 import { Points, PointMaterial, Preload } from "@react-three/drei"
 import * as random from "maath/random/dist/maath-random.esm"
 
