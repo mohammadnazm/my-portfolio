6031d62534cb1729eeaac4e76ff73a68903d5cc9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 17:34:00 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,8 +44,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Software Developer with self earned experience in both front-end and
-        back-end development.
+        Software Developer with experience in both front-end and back-end
+        development.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
