518a125fb2aada4718bede329db9865764c7a57f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 11:41:01 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,47 +27,53 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <p className={styles.sectionHeadText}>Contact.</p>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Mohammad Nazm</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name"
+              className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            ></input>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email"
+              className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            ></input>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              type="text"
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say"
+              className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </label>
+        </form>
       </motion.dev>
-      <form
-        ref={formRef}
-        onSubmit={handleSubmit}
-        className="mt-12 flex flex-col gap-8"
-      >
-        <label className="flex flex-col">
-          <span className="text-white font-medium mb-4">Mohammad Nazm</span>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            placeholder="What's your name"
-            className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
-          ></input>
-        </label>
-        <label className="flex flex-col">
-          <span className="text-white font-medium mb-4">Your Email</span>
-          <input
-            type="email"
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-            placeholder="What's your email"
-            className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
-          ></input>
-        </label>
-        <label className="flex flex-col">
-          <span className="text-white font-medium mb-4">Your Message</span>
-          <textarea
-            type="text"
-            rows="7"
-            name="message"
-            value={form.message}
-            onChange={handleChange}
-            placeholder="What do you want to say"
-            className="bt-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
-          ></textarea>
-        </label>
-      </form>
     </div>
   )
 }
