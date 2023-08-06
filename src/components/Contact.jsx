610c01984012ebed000fd696b1bf86eb94736174
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 11:43:15 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -74,6 +74,12 @@ const Contact = () => {
           </label>
         </form>
       </motion.dev>
+      <motion.dev
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.dev>
     </div>
   )
 }
