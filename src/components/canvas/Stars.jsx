0b59530d0fb9e16b3a49b3caab320ee9e19c2f4e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 13:25:31 +0300

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react"
+import { Canvas, useFrame } from "react-three-fiber"
+import { Points, PointMaterial, Preload } from "@react-three/drei"
 
 const Stars = () => {
   return (
-    <div>Stars</div>
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+      </Canvas>
+    </div>
   )
 }
 
