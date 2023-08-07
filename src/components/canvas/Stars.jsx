589ed80961c6d60eebf86d6921705e6a00097f77
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-08 00:38:53 +0300

    change logo

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -32,7 +32,7 @@ const Stars = props => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
-      <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas cmera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
